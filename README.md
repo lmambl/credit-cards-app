2abcbbfc389a822119ccc3d9d48804a612135138
lmambl <lmambl@mail.ru>
2024-01-28 13:45:09 +0300

    docs: readme update

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# React + Vite
+# Credit Card Animation
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+1. fork repo
+2. npm i
+3. npm run dev
+4. Удерживайте 0,5с чтобы увидеть анимацию переворота
+5. При нажатии на баланс карты применяется State, который добавляет или удаляет в ClassName свойство filter
--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,8 @@ import './App.css';
 import CreditCardFront from './components/CreditCardFront/CreditCardFront';
 import CreditCardBack from './components/CreditCardBack/CreditCardBack';
 import CreditCardMain from './components/CreditCardMain/CreditCardMain';
-// import { useState } from 'react';
-function App() {
-	// const [showBalance, setShowBalance] = useState(true); // Изначально показываем баланс
 
-	// const handleCheckboxChange = () => {
-	//     setShowBalance(!showBalance); // Изменяем состояние чекбокса
-	// };
+function App() {
 	return (
 		<>
 			<div className="flip-card">
@@ -22,12 +17,6 @@ function App() {
 					</div>
 				</div>
 			</div>
-			{/* <div>
-				<label htmlFor="">
-					Изменить видимость элементов <input type="checkbox" checked={showBalance} onChange={handleCheckboxChange} />
-				</label>
-				showBalance={showBalance}
-			</div> */}
 		</>
 	);
 }
